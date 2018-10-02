3fb3e5c03523c0c1bd1dcd975f43f306769db5c5
Fal-cod <abdullahfalus@gmail.com>
2018-10-02 14:04:16 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'CDREM-Dallah-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Dallah-forms';
+}
